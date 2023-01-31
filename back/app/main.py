5dc8caaedd5d80d65763516b067934de0c76b7fa
ismailozdel <ismailozdel@gmail.com>
2023-01-31 21:04:52 +0300

    a

--- back/app/main.py
+++ back/app/main.py
@@ -1,5 +1,5 @@
 from flask import Flask, request,jsonify
-from flask_socketio import SocketIO,emit,join_room,send,leave_room,rooms
+from flask_socketio import SocketIO,emit,join_room,send,leave_room
 from flask_cors import CORS
 from time import sleep
 import sounddevice as sd
@@ -9,31 +9,82 @@ app.config['SECRET_KEY'] = 'secret!'
 CORS(app,resources={r"/*":{"origins":"*"}})
 socketio = SocketIO(app,cors_allowed_origins="*")
 
+# dictionary to store room membership
+users = {}
+rooms = {}
+file = open("asd.txt","a+b")
+@socketio.on('join room')
+def handle_join_room(data):
+    print(data)
+    room_name = data['room_name']
+    join_room(room_name)
+    client_id = request.sid
 
-roomss = []
+    # create the room if it doesn't exist
+    if room_name not in rooms:
+        rooms[room_name] = []
+    # add the client to the room
+    rooms[room_name].append(client_id)
+
+@socketio.on('register')
+def register(data):
+    user_name = data['user_name']
+    client_id = request.sid
+    if user_name not in users:
+        users[user_name] = []
+    users[user_name].append(client_id)
+    print(users)
+
+@socketio.on("send notification")
+def notification(data):
+    target = data['target_user']
+    user = data['user_name']
+    if user in users and target in users:
+        emit('notification',{'notification':user},to=users[target])
+
+@socketio.on('audio_data')
+def handle_audio_data(data):
+    global file
+    file.write(data['audio'])
+    print(data['audio'])
+    emit('audio_data', data['audio'],include_self=False,to=data['room'])
+    """print(data)
+    client_id = request.sid
+    # find the room that the client is in
+    for room_name, clients in rooms.items():
+        if client_id in clients:
+            # broadcast the audio data to all clients in the room
+            emit('audio_data', data['data'], room=room_name,include_self=True)
+            break"""
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True,port=8000)
+
+
+"""roomss = []
 users = []
 @app.route("/http-call")
 def http_call():
-    """return JSON with string data as the value"""
+    """"""return JSON with string data as the value""""""
     data = {'data':'This text was fetched using an HTTP call to server on render'}
     return jsonify(data)
 
 @socketio.on("connect")
 def connected():
-    """event listener when client connects to the server"""
+    """"""event listener when client connects to the server""""""
     #print(request.sid)
     #print("client has connected")
     emit("connect",{"data":f"id: {request.sid} is connected"})
 
 @socketio.on('data')
 def handle_message(data):
-    """event listener when client types a message"""
+    """"""event listener when client types a message""""""
     #print("data from the front end: ",str(data))
     emit("data",{'data':data,'id':request.sid},broadcast=True)
 a = 0
 @socketio.on("disconnect")
 def disconnected():
-    """event listener when client disconnects to the server"""
+    """"""event listener when client disconnects to the server""""""
     #print("user disconnected")
     emit("disconnect",f"user {request.sid} disconnected",broadcast=True)
 
@@ -61,17 +112,17 @@ def on_leave(data):
 def handle_audio_data(data):
     # process the audio data and broadcast it to all connected clients
     #[x] = struct.unpack('f',data)
-    """print("------------------------------")
+    print("------------------------------")
     print(rooms)
     print("------------------------------")
     room = list(filter(lambda user: user["sid"] == request.sid,rooms))
     print("///////////////////////////////")
     print(room)
-    print("///////////////////////////////")"""
+    print("///////////////////////////////")
     room = rooms()[0]
     print(room)
     print(rooms())
     emit('audio_data', data['audio'], broadcast=False,include_self=False,to=room)
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True,port=8000)
+    socketio.run(app, debug=True,port=8000)"""
--- front/client/src/App.js
+++ front/client/src/App.js
@@ -1,4 +1,157 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'
+import io from 'socket.io-client'
+
+const socket = io('http://localhost:8000/', {
+  transports: ['websocket'],
+  cors: {
+    origin: "http://localhost:3000/"
+  }
+})
+/*
+const socket = io('https://test.heynova.work/', {
+  transports: ['websocket'],
+  cors: {
+    origin: "https://test2.heynova.work/"
+  }
+})*/
+
+function App() {
+  const [audioContext, setAudioContext] = useState(null)
+  const [microphoneStream, setMicrophoneStream] = useState(null)
+  const [roomName, setRoomName] = useState('')
+  const [isConnected, setIsConnected] = useState(false)
+  const [isMuted, setIsMuted] = useState(false)
+
+  const [userName, setUserName] = useState('')
+  const [targetUser, setTargetUser] = useState('')
+
+  useEffect(() => {
+    if (isConnected) {
+      async function init() {
+        try {
+          // initialize audio context and get microphone input stream
+          const audioCtx = new AudioContext()
+          const stream = await navigator.mediaDevices.getUserMedia({ audio: true })
+          setAudioContext(audioCtx)
+          setMicrophoneStream(stream)
+        } catch (error) {
+          console.error(error)
+        }
+      }
+      init()
+    }
+  }, [isConnected])
+
+  useEffect(() => {
+    if (!audioContext || !microphoneStream) {
+      return
+    }
+
+    // create audio nodes
+    const microphoneInput = audioContext.createMediaStreamSource(microphoneStream)
+    const processor = audioContext.createScriptProcessor(1024, 1, 1)
+
+    // connect the nodes
+    microphoneInput.connect(processor)
+    processor.connect(audioContext.destination)
+
+    // handle audio data
+    processor.onaudioprocess = (event) => {
+      // get microphone output data
+      const microphoneOutput = event.inputBuffer.getChannelData(0)
+      // send it to the server over the Socket.IO connection
+      if (!isMuted && roomName) {
+        socket.emit('audio_data', { "audio": microphoneOutput, "room": roomName })
+      }
+    }
+
+    // handle audio data received from the server
+    socket.on('audio_data', (data) => {
+      const test = new Float32Array(data)
+      console.log("-----")
+      console.log(test)
+      console.log("//")
+      // create an audio buffer from the received data
+      const audioBuffer = audioContext.createBuffer(1, test.length, audioContext.sampleRate)
+      audioBuffer.copyToChannel(new Float32Array(data), 0)
+      // create an audio source from the buffer
+      const source = audioContext.createBufferSource()
+      source.buffer = audioBuffer
+      // connect the source to the audio context destination
+      source.connect(audioContext.destination)
+      // play the audio
+      source.start()
+    })
+    socket.on('notification',(data)=>{
+      console.log(1)
+      console.log(data)
+      console.log(data['notification'])
+    })
+
+    return () => {
+      // disconnect the audio nodes and close the audio context
+      microphoneInput.disconnect()
+      processor.disconnect()
+      audioContext.close()
+    }
+  }, [audioContext, microphoneStream, roomName, isMuted])
+
+  function joinRoom() {
+    socket.emit('join room', { room_name: roomName })
+    setIsConnected(true)
+  }
+  function register() {
+    socket.emit('register', { user_name: userName })
+  }
+  function sendNotification() {
+    socket.emit('send notification', { user_name: userName, target_user: targetUser })
+  }
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Enter room name"
+        value={roomName}
+        onChange={(e) => setRoomName(e.target.value)}
+      />
+      <button onClick={joinRoom}>Join Room</button>
+      <input
+        type="text"
+        placeholder="enter user name"
+        value={userName}
+        onChange={(e) => setUserName(e.target.value)}
+      />
+      <button onClick={register}>Register</button>
+      <div>
+        <input
+          type="text"
+          placeholder="enter user name"
+          value={targetUser}
+          onChange={(e) => setTargetUser(e.target.value)} />
+        <button onClick={sendNotification}>send notification</button>
+      </div>
+      
+      {isConnected ? (
+        <div>
+          <button onClick={() => setIsMuted(!isMuted)}>
+            {isMuted ? 'Unmute' : 'Mute'}
+          </button>
+          <button onClick={() => {
+            setIsConnected(false)
+            setRoomName('')
+            socket.emit('leave room', roomName)
+          }
+          }>Leave Room</button>
+        </div>
+      ) : null}
+    </div>
+  )
+}
+export default App;
+
+
+/*import { useEffect, useState } from 'react';
 import './App.css';
 import HttpCall from './components/HttpCall';
 import WebSocketCall from './components/WebSocketCall';
@@ -11,7 +164,6 @@ function App() {
   const [loading,setLoading] = useState(true)
   const [buttonStatus,setButtonStatus] = useState(false)
   const [voiceButtonStatus,setVoiceButtonStatus] = useState(false)
- 
   useEffect(() => {
     if(buttonStatus === true){
        const socket = io("http://localhost:8000",{
@@ -85,4 +237,4 @@ function App() {
   );
 }
 
-export default App;
+export default App;*/
